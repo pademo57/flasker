34a6210871b8da8bd6f8392072fa38438699d2d1
Paul Morphy <pademo57@gmail.com>
2021-02-06 11:35:19 -0500

    Changed 500 file

--- hello.py
+++ hello.py
@@ -32,7 +32,7 @@ def user(name):
 def page_not_found(e):
     return render_template("404.html"), 404
 
-# Internal Server Error
+# Internal Server Error Page
 @app.errorhandler(500)
 def page_not_found(e):
     return render_template("500.html"), 500
